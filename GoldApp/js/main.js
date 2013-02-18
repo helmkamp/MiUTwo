90186c91999f2c82e066c057e8f7e2e17eae5386
Andrew Helmkamp <helmy@threebeersaday.com>
2013-02-18 17:36:32 -0600

    Updated add/edit forms

--- GoldApp/js/main.js
+++ GoldApp/js/main.js
@@ -43,7 +43,7 @@ var editItem = function(urlObj, options) {
 			if(item.highlighted[1] === "Yes") {
 				$('#editHighlight').attr("checked", "checked");
 			}
-			$('#comments').val(item.comments[1]);
+			$('#editComments').val(item.comments[1]);
 			$('#hiddenKey').val(itemKey);
 			console.log(item);
 		
@@ -78,6 +78,11 @@ function refreshPage() {
 		reloadPage: true
 	});
 }
+
+$('#cancelEdit').click(function() {
+	history.back();
+	return false;
+});
 
 // Listen for any attempts to call changePage().
 $(document).on("pagebeforechange", function(e, data) {
